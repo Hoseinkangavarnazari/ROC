dc6b1e1351ad8f5ec2202fbfa7adae75a9e7b77a
amirhosseinnazari <hnirsnazari@yahoo.com>
2018-03-31 16:33:05 +0430

    Adding Module for local development and createing gitignore for modules in react and node

---
+++ react-ui/src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+
+    state = {
+        hasError: false,
+        errorMessage: ''
+    }
+
+    componentDidCatch = (error, info) => { 
+        this.setState({
+            hasError: true,
+            errorMessage: error
+        });
+    }
+
+    render() {
+
+        if (this.state.hasError) {
+            return <h1>Something went wrong </h1>;
+        } else {
+            return this.props.children;
+        }
+
+
+    }
+}
+
+
+export default ErrorBoundary ; 
--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+registerServiceWorker();
