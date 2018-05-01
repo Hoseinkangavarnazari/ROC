ec9e7ddb93a3722d33b5b286122c08e480994194
Ashkan R. Nejad <rahmani.nejad.ali@gmail.com>
2018-05-01 17:21:43 +0430

    prototypal google Authentcation

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,23 +1,22 @@
+'use strict';
 var express = require('express');
 
-// Require controller modules.
 var usersController = require('../controllers/usersController');
-
+var router = require('express').Router();
 
 
 module.exports = (function () {
-    'use strict';
-    var router = require('express').Router();
 
 
     // GET catalog home page.
     router.get('/allContacts', usersController.allContacts);
 
     // GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
+    //TODO -> implement
     router.get('/oneContactInfo', usersController.oneUser);
 
     // POST request for create contact
-    router.post('/SignUpUser', usersController.createUser);
+    //router.post('/SignUpUser', usersController.createUser);   //PreDeletion --> handeled in googAuth method 
 
     // POST request for create contact
     router.post('/addContact', usersController.addContact);
@@ -25,5 +24,12 @@ module.exports = (function () {
     // DELETE request for eliminate contact
     router.delete('/deleteContact', usersController.deleteContact);
 
+   
     return router;
 })();
+
+
+module.exports.googAuth = function(token, profileID, name, email, done){
+   usersController.googAuth(token, profileID, name, email, done);
+}
+
